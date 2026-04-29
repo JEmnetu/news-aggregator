5d59e0469da4183f9feec04524655a93bf24e355
Jacob Habtemariam <jacob@mac.myfiosgateway.com>
2026-04-28 20:32:30 -0400

    queries DB for articles and saves them to local state

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,29 @@
+import { useState, useEffect } from "react"
+import { getNews } from "../services/api"
+import { NewsResponse } from "../types/NewsResponse"
 
 const HomePage = () => {
 
+    const [news, setNews] = useState<NewsResponse>()
+    const [loading, setLoading] = useState<boolean>(true)
+
+    const getNewsArticles = async () => {
+      
+        const response = await getNews();
+        setNews(response)
+    }
+
+    useEffect(() => {
+        const load = async () => {
+            await getNewsArticles()
+            setLoading(false)
+        }
+        load()
+    }, [])
 
     return <>
-       
         <h1>Home Page</h1>
+        {!loading && console.log(news)}
     </>
 }
 
